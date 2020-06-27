60cf28eebae34ccf392479e397969b729c2017b2
Raghav Surya <raghav.surya@outlook.com>
2020-06-27 12:34:10 +0100

    AtoI String to Integere

---
+++ AtoI/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace AtoI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyAtoi(" -4000"));
+        }
+
+        public static int MyAtoi(string str)
+        {
+            var trimmedString = str.Trim(' ');
+            char[] strToChar = trimmedString.ToCharArray();
+            StringBuilder sb = new StringBuilder();
+            string[] allowedChars = { "+", "-" };
+
+            foreach (char c in strToChar)
+            {
+                if ((!allowedChars.Contains(c) && Char.GetNumericValue(c) == -1)
+                   || (sb.ToString().Length > 0 && Char.GetNumericValue(c) == -1))
+                {
+                    break;
+                }
+                sb.Append(c);
+            }
+
+            var possibleNumericStringWithOrWithoutSign = sb.ToString();
+            if (!string.IsNullOrEmpty(possibleNumericStringWithOrWithoutSign))
+            {
+                bool isNegative = possibleNumericStringWithOrWithoutSign.StartsWith("-");
+                try
+                {
+
+                    return Int32.Parse(possibleNumericStringWithOrWithoutSign);
+                }
+                catch (System.FormatException ex)
+                {
+                    return 0;
+                }
+                catch (System.OverflowException)
+                {
+                    return isNegative ? Int32.MinValue : Int32.MaxValue;
+                }
+            }
+            return 0;
+        }
+    }
+}
