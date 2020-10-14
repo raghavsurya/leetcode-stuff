54758a02ae0722188a0c2daf84e3b5351cabf267
raghavsurya <raghav.surya@outlook.com>
2020-10-14 19:28:04 +0100

    Create SymmetricTreeTraversalIterativeQueue.java

---
+++ SymmetricTreeTraversalIterativeQueue.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        queue.add(root);
+        
+        while(!queue.isEmpty()) {
+            TreeNode leftNode = queue.poll();
+            TreeNode rightNode = queue.poll();
+            if(leftNode == null && rightNode == null) continue;
+            if(leftNode == null || rightNode == null) return false;
+            if(leftNode.val != rightNode.val) return false;
+            queue.add(leftNode.left);
+            queue.add(rightNode.right);
+            queue.add(leftNode.right);
+            queue.add(rightNode.left);
+            
+        }
+        return true;
+    }
+}
