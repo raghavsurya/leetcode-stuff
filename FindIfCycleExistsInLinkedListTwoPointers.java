215b73d0bb83b1c7d17f05baf39788631059999e
raghavsurya <raghav.surya@outlook.com>
2020-11-08 11:59:52 +0000

    Create FindIfCycleExistsInLinkedListTwoPointers.java

---
+++ FindIfCycleExistsInLinkedListTwoPointers.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(slow != fast) {
+            if(fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
+//Space complexity: O(1) memory We only use two nodes (slow and fast) so the space complexity is O(1)O(1).
+//Time complexity: O(N+K) which is O(n)
