7ceafef89962d2f2a7a954fc8f0d10429139adae
raghavsurya <raghav.surya@outlook.com>
2020-11-15 19:12:39 +0000

    Create MergeSortedListsBetterIterative.java

---
+++ MergeSortedListsBetterIterative.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode preHead = new ListNode(-1);
+        ListNode prev = preHead;
+        
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                 prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2: l1;
+        return preHead.next;
+    }
+   
+    
+}
+//TC: O(n + m) cos the loop runs for sum of length of the 2 lists
+//SC: O(1) not much use of pointers here
